1357225049c8d20decf2c0b95a16a7670fed44b9
José Pedro <jpedrolmail@gmail.com>
2017-10-13 18:14:45 +0100

    Removed RuntimeEngine.

--- source/Cosmos.IL2CPU/ILScanner.cs
+++ source/Cosmos.IL2CPU/ILScanner.cs
@@ -219,8 +219,6 @@ namespace Cosmos.IL2CPU
             ILOp.mPlugManager = mPlugManager;
 
             // Pull in extra implementations, GC etc.
-            Queue(RuntimeEngineRefs.InitializeApplicationRef, null, "Explicit Entry");
-            Queue(RuntimeEngineRefs.FinalizeApplicationRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.SetTypeInfoRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.SetMethodInfoRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.SetInterfaceInfoRef, null, "Explicit Entry");
@@ -283,8 +281,6 @@ namespace Cosmos.IL2CPU
             ILOp.mPlugManager = mPlugManager;
 
             // Pull in extra implementations, GC etc.
-            Queue(RuntimeEngineRefs.InitializeApplicationRef, null, "Explicit Entry");
-            Queue(RuntimeEngineRefs.FinalizeApplicationRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.SetMethodInfoRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.IsInstanceRef, null, "Explicit Entry");
             Queue(VTablesImplRefs.SetTypeInfoRef, null, "Explicit Entry");
