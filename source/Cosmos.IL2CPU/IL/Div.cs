e4a17523394ba954b49b9d0203944f18fb88eedc
José Pedro <jpedrolmail@gmail.com>
2018-03-02 17:57:02 +0000

    Fixed div and rem for int64 operands.

--- source/Cosmos.IL2CPU/IL/Div.cs
+++ source/Cosmos.IL2CPU/IL/Div.cs
@@ -1,30 +1,27 @@
 using System;
-using XSharp.Assembler.x86.SSE;
 
 using XSharp;
 using static XSharp.XSRegisters;
-using CPUx86 = XSharp.Assembler.x86;
-using Label = XSharp.Assembler.Label;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-    [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Div )]
+    [OpCode(ILOpCode.Code.Div)]
     public class Div : ILOp
     {
-        public Div( XSharp.Assembler.Assembler aAsmblr )
-            : base( aAsmblr )
+        public Div(XSharp.Assembler.Assembler aAsmblr)
+            : base(aAsmblr)
         {
         }
 
-        public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode )
+        public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
         {
-			var xStackItem = aOpCode.StackPopTypes[0];
+            var xStackItem = aOpCode.StackPopTypes[0];
             var xStackItemSize = SizeOfType(xStackItem);
             var xStackItem2 = aOpCode.StackPopTypes[0];
             var xStackItem2Size = SizeOfType(xStackItem2);
-			if (xStackItemSize == 8)
+            if (xStackItemSize == 8)
             {
-				// there seem to be an error in MS documentation, there is pushed an int32, but IL shows else
+                // there seem to be an error in MS documentation, there is pushed an int32, but IL shows else
                 if (xStackItem2Size != 8)
                 {
                     throw new Exception("Cosmos.IL2CPU.x86->IL->Div.cs->Error: Expected a size of 8 for Div!");
@@ -39,106 +36,32 @@ namespace Cosmos.IL2CPU.X86.IL
                 }
                 else
                 {
-					string BaseLabel = GetLabel(aMethod, aOpCode) + ".";
-					string LabelShiftRight = BaseLabel + "ShiftRightLoop";
-					string LabelNoLoop = BaseLabel + "NoLoop";
-					string LabelEnd = BaseLabel + "End";
-
-					// divisor
-					//low
-					XS.Set(ESI, ESP, sourceIsIndirect: true);
-					//high
-					XS.Set(EDI, ESP, sourceDisplacement: 4);
-
-					// pop both 8 byte values
-					XS.Add(ESP, 8);
-
-					//dividend
-					// low
-					XS.Set(EAX, ESP, sourceIsIndirect: true);
-					//high
-					XS.Set(EDX, ESP, sourceDisplacement: 4);
-
+                    XS.FPU.IntLoad(ESP, displacement: 8, size: RegisterSize.Long64);
+                    XS.FPU.IntLoad(ESP, isIndirect: true, size: RegisterSize.Long64);
                     XS.Add(ESP, 8);
 
-					// set flags
-					XS.Or(EDI, EDI);
-					// if high dword of divisor is already zero, we dont need the loop
-					XS.Jump(CPUx86.ConditionalTestEnum.Zero, LabelNoLoop);
+                    XS.LiteralCode("fdivp st1, st0");
 
-					// set ecx to zero for counting the shift operations
-					XS.Xor(ECX, ECX);
-
-					XS.Label(LabelShiftRight);
-
-					// shift divisor 1 bit right
-                    XS.ShiftRightDouble(ESI, EDI, 1);
-					XS.ShiftRight(EDI, 1);
-
-					// increment shift counter
-					XS.Increment(ECX);
-
-					// set flags
-					XS.Or(EDI, EDI);
-					// loop while high dword of divisor till it is zero
-					XS.Jump(CPUx86.ConditionalTestEnum.NotZero, LabelShiftRight);
-
-					// shift the divident now in one step
-					// shift divident CL bits right
-					XS.ShiftRightDouble(EAX, EDX, CL);
-					XS.ShiftRight(EDX, CL);
-
-					// so we shifted both, so we have near the same relation as original values
-					// divide this
-					XS.IntegerDivide(ESI);
-
-					// sign extend
-					XS.SignExtendAX(RegisterSize.Int32);
-
-					// save result to stack
-					XS.Push(EDX);
-					XS.Push(EAX);
-
-					//TODO: implement proper derivation correction and overflow detection
-
-					XS.Jump(LabelEnd);
-
-					XS.Label(LabelNoLoop);
-					//save high dividend
-					XS.Set(ECX, EAX);
-					XS.Set(EAX, EDX);
-					// extend that sign is in edx
-					XS.SignExtendAX(RegisterSize.Int32);
-					// divide high part
-					XS.IntegerDivide(ESI);
-					// save high result
-					XS.Push(EAX);
-					XS.Set(EAX, ECX);
-					// divide low part
-					XS.Divide(ESI);
-					// save low result
-					XS.Push(EAX);
-
-					XS.Label(LabelEnd);
+                    XS.FPU.IntStoreWithTruncate(ESP, true, RegisterSize.Long64);
                 }
             }
             else
             {
-				if (TypeIsFloat(xStackItem))
+                if (TypeIsFloat(xStackItem))
                 {
                     XS.SSE.MoveSS(XMM0, ESP, sourceIsIndirect: true);
-                    XS.Add(XSRegisters.ESP, 4);
+                    XS.Add(ESP, 4);
                     XS.SSE.MoveSS(XMM1, ESP, sourceIsIndirect: true);
                     XS.SSE.DivSS(XMM1, XMM0);
                     XS.SSE.MoveSS(ESP, XMM1, destinationIsIndirect: true);
                 }
                 else
                 {
-                    XS.Pop(XSRegisters.ECX);
-                    XS.Pop(XSRegisters.EAX);
-					XS.SignExtendAX(RegisterSize.Int32);
-                    XS.IntegerDivide(XSRegisters.ECX);
-                    XS.Push(XSRegisters.EAX);
+                    XS.Pop(ECX);
+                    XS.Pop(EAX);
+                    XS.SignExtendAX(RegisterSize.Int32);
+                    XS.IntegerDivide(ECX);
+                    XS.Push(EAX);
                 }
             }
         }
