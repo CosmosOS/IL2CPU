6354523a587182d2688c6717c34994bbf0233ee7
José Pedro <jpedrolmail@gmail.com>
2018-04-02 19:37:38 +0100

    Added IsolatedAssemblyLoadContext.

---
+++ source/Cosmos.IL2CPU/AssemblyIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cosmos.IL2CPU
+{
+    internal class AssemblyIdentity : IEquatable<AssemblyIdentity>
+    {
+        private AssemblyName _assemblyName;
+
+        public AssemblyIdentity(AssemblyName assemblyName)
+        {
+            _assemblyName = assemblyName;
+        }
+
+        public bool Equals(AssemblyIdentity other) => _assemblyName.FullName == other._assemblyName.FullName;
+        public override bool Equals(object obj) => obj is AssemblyIdentity other && Equals(other);
+        public override int GetHashCode() => _assemblyName.FullName.GetHashCode();
+        public override string ToString() => _assemblyName.FullName;
+    }
+}
