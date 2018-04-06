1504df02ffc9223df56d3c4a35dc5ab497903173
José Pedro <jpedrolmail@gmail.com>
2018-04-06 18:28:13 +0100

    Changed AssemblyIdentity so that only the assembly name is compared.

--- source/Cosmos.IL2CPU/AssemblyIdentity.cs
+++ source/Cosmos.IL2CPU/AssemblyIdentity.cs
@@ -3,6 +3,10 @@ using System.Reflection;
 
 namespace Cosmos.IL2CPU
 {
+    /// <summary>
+    /// Wrapper for <seealso cref="AssemblyName" />, used to compare <seealso cref="AssemblyName" /> objects equality.
+    /// Only the assembly name is compared (<seealso cref="AssemblyName.Name" />).
+    /// </summary>
     internal class AssemblyIdentity : IEquatable<AssemblyIdentity>
     {
         private AssemblyName _assemblyName;
@@ -12,9 +16,9 @@ namespace Cosmos.IL2CPU
             _assemblyName = assemblyName;
         }
 
-        public bool Equals(AssemblyIdentity other) => _assemblyName.FullName == other._assemblyName.FullName;
+        public bool Equals(AssemblyIdentity other) => _assemblyName.Name == other._assemblyName.Name;
         public override bool Equals(object obj) => obj is AssemblyIdentity other && Equals(other);
-        public override int GetHashCode() => _assemblyName.FullName.GetHashCode();
-        public override string ToString() => _assemblyName.FullName;
+        public override int GetHashCode() => _assemblyName.Name.GetHashCode();
+        public override string ToString() => _assemblyName.Name;
     }
 }
---
+++ tests/IL2CPU.Compiler.Tests/TypeResolverTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Cosmos.IL2CPU;
+
+namespace IL2CPU.Compiler.Tests
+{
+    [TestFixture(TestOf = typeof(TypeResolver))]
+    public class TypeResolverTests
+    {
+        [Test]
+        public void ResolveType_ForTypeName_ThrowsNullReferenceException()
+        {
+            var assemblyLoadContext = GetAssemblyLoadContext();
+            var typeResolver = new TypeResolver(assemblyLoadContext);
+
+            Assert.Throws<NullReferenceException>(() => typeResolver.ResolveType("TypeResolverTests"));
+        }
+
+        [Test]
+        public void ResolveType_ForFullTypeNameWithoutAssemblyPublicKeyToken_WorksForStrongNameSignedAssembly()
+        {
+            var assemblyLoadContext = GetAssemblyLoadContext();
+            var typeResolver = new TypeResolver(assemblyLoadContext);
+
+            // TODO: test assembly which is not loaded in the Default AssemblyLoadContext
+            Assert.That(typeResolver.ResolveType("IL2CPU.Compiler.Tests.TypeResolverTests, IL2CPU.Compiler.Tests"), Is.Not.Null);
+        }
+
+        private IsolatedAssemblyLoadContext GetAssemblyLoadContext() => new IsolatedAssemblyLoadContext(GetAssemblies());
+
+        private IEnumerable<string> GetAssemblies()
+        {
+            yield return typeof(TypeResolverTests).Assembly.Location;
+        }
+    }
+}
