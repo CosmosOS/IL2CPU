e01f98a63c884d85d621f28abc2108b6a73bb9cf
José Pedro <jpedrolmail@gmail.com>
2018-04-07 22:23:17 +0100

    Code cleanup.

--- source/Cosmos.IL2CPU/IL/Refanytype.cs
+++ source/Cosmos.IL2CPU/IL/Refanytype.cs
@@ -1,38 +1,20 @@
 using System;
 
+using XSharp.Assembler;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-	[Cosmos.IL2CPU.OpCode(ILOpCode.Code.Refanytype)]
-	public class Refanytype: ILOp
-	{
-		public Refanytype(XSharp.Assembler.Assembler aAsmblr):base(aAsmblr)
-		{
-		}
+    [OpCode(ILOpCode.Code.Refanytype)]
+    public class Refanytype : ILOp
+    {
+        public Refanytype(Assembler aAsmblr)
+            : base(aAsmblr)
+        {
+        }
 
-    public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode) {
-      throw new NotImplementedException();
+        public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    
-		// using System;
-		// using System.IO;
-		// 
-		// 
-		// using CPU = XSharp.Assembler.x86;
-		// 
-		// namespace Cosmos.IL2CPU.IL.X86 {
-		// 	[XSharp.Assembler.OpCode(OpCodeEnum.Refanytype)]
-		// 	public class Refanytype: Op {
-		// 		public Refanytype(ILReader aReader, MethodInformation aMethodInfo)
-		// 			: base(aReader, aMethodInfo) {
-		// 		}
-		// 		public override void DoAssemble() {
-		// //			Pop("eax");
-		// //			Pushd("[eax]");
-		// 		}
-		// 	}
-		// }
-		
-	}
 }
