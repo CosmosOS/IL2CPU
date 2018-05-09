ed10034cdf6d74d94b027bee2a60527db928c6b8
José Pedro <jpedrolmail@gmail.com>
2018-05-09 23:40:26 +0100

    Code cleanup.

--- source/Cosmos.IL2CPU/MethodBaseComparer.cs
+++ source/Cosmos.IL2CPU/MethodBaseComparer.cs
@@ -1,30 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Reflection;
+
 using Cosmos.IL2CPU.Extensions;
 
 namespace Cosmos.IL2CPU
 {
     public class MethodBaseComparer : IComparer<MethodBase>, IEqualityComparer<MethodBase>
     {
-        #region IComparer<MethodBase> Members
-        public int Compare(MethodBase x, MethodBase y)
-        {
-            return x.GetFullName().CompareTo(y.GetFullName());
-        }
-        #endregion
+        public int Compare(MethodBase x, MethodBase y) =>
+            String.Compare(x.GetFullName(), y.GetFullName(), StringComparison.Ordinal);
 
-        public bool Equals(MethodBase x, MethodBase y)
-        {
-            return Compare(x, y) == 0;
-        }
+        public bool Equals(MethodBase x, MethodBase y) =>
+            String.Equals(x.GetFullName(), y.GetFullName(), StringComparison.Ordinal);
 
-        public int GetHashCode(MethodBase obj)
-        {
-            return obj.GetFullName().GetHashCode();
-        }
+        public int GetHashCode(MethodBase obj) => obj.GetFullName().GetHashCode();
     }
-
 }
