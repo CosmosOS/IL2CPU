c69cb3eca4cb695d37e0a3006197311d2942a7cd
José Pedro <jpedrolmail@gmail.com>
2018-10-04 23:41:34 +0100

    Code cleanup.

--- source/IL2CPU.API/Attribs/PlugMethod.cs
+++ source/IL2CPU.API/Attribs/PlugMethod.cs
@@ -3,12 +3,12 @@ using System;
 namespace IL2CPU.API.Attribs {
   [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
   public sealed class PlugMethod: Attribute {
-    public string Signature = null;
-    public bool Enabled = true;
-    public Type Assembler = null;
-    public bool PlugRequired = false;
-    public bool IsWildcard = false;
-    public bool WildcardMatchParameters = false;
-    public bool IsOptional = true;
+    public string Signature { get; set; }
+    public bool Enabled { get; set; } = true;
+    public Type Assembler { get; set; }
+    public bool PlugRequired { get; set; }
+    public bool IsWildcard { get; set; }
+    public bool WildcardMatchParameters { get; set; }
+    public bool IsOptional { get; set; } = true;
   }
 }
