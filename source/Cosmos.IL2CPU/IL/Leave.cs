23d052b9478c80c549ebbf1e5827b591fee7efaf
Charles Betros <charles.betros@gmail.com>
2020-07-07 13:20:20 -0500

    Check BugMode before emitting all labels

--- source/Cosmos.IL2CPU/IL/Leave.cs
+++ source/Cosmos.IL2CPU/IL/Leave.cs
@@ -1,31 +1,35 @@
 using System.Reflection.Metadata;
-
 using Cosmos.IL2CPU.Extensions;
-
 using XSharp;
 using static XSharp.XSRegisters;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-    [OpCode(ILOpCode.Code.Leave)]
-    public class Leave : ILOp
+  [OpCode(ILOpCode.Code.Leave)]
+  public class Leave : ILOp
+  {
+    public Leave(XSharp.Assembler.Assembler aAsmblr)
+        : base(aAsmblr)
     {
-        public Leave(XSharp.Assembler.Assembler aAsmblr)
-            : base(aAsmblr)
-        {
-        }
-
-        public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
-        {
-            // apparently, Roslyn changed something to the output. We now have to figure out where to jump to.
-            if (aOpCode.CurrentExceptionRegion.Kind.HasFlag(ExceptionRegionKind.Finally)
-                && aOpCode.CurrentExceptionRegion.HandlerOffset > aOpCode.Position)
-            {
-                XS.Set(aMethod.MethodBase.GetFullName() + "_" + "LeaveAddress_" + aOpCode.CurrentExceptionRegion.HandlerOffset.ToString("X2"), Assembler.CurrentIlLabel + "." + (Assembler.AsmIlIdx + 2).ToString("X2"), destinationIsIndirect: true, size: RegisterSize.Int32);
-                XS.Jump(AppAssembler.TmpPosLabel(aMethod, aOpCode.CurrentExceptionRegion.HandlerOffset));
-            }
-
-            XS.Jump(AppAssembler.TmpBranchLabel(aMethod, aOpCode));
-        }
     }
+
+    public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
+    {
+      string jumpTarget = AppAssembler.TmpBranchLabel(aMethod, aOpCode);
+
+      // apparently, Roslyn changed something to the output. We now have to figure out where to jump to.
+      if (aOpCode.CurrentExceptionRegion.Kind.HasFlag(ExceptionRegionKind.Finally)
+          && aOpCode.CurrentExceptionRegion.HandlerOffset > aOpCode.Position)
+      {
+        string destination = $"{aMethod.MethodBase.GetFullName()}_LeaveAddress_{aOpCode.CurrentExceptionRegion.HandlerOffset:X2}";
+        string source = AppAssembler.TmpBranchLabel(aMethod, aOpCode);
+        XS.Set(destination, source, destinationIsIndirect: true, size: RegisterSize.Int32);
+        XS.Jump(AppAssembler.TmpPosLabel(aMethod, aOpCode.CurrentExceptionRegion.HandlerOffset));
+      }
+      else
+      {
+        XS.Jump(jumpTarget);
+      }
+    }
+  }
 }
