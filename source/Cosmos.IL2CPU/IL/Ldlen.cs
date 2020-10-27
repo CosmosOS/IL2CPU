f3ade2b7b6a0559f1822c9cef90111d34a5779b9
Quajak <jasper.gerigk@gmx.net>
2020-10-27 17:50:42 +0100

    Fix/stelem ref index check (#100)
    * Fix Stelem_ref index out of bound checks
    
    * Use correct comparison

--- source/Cosmos.IL2CPU/IL/Ldlen.cs
+++ source/Cosmos.IL2CPU/IL/Ldlen.cs
@@ -5,30 +5,30 @@ using static XSharp.XSRegisters;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-            [OpCode(ILOpCode.Code.Ldlen)]
-            public class Ldlen : ILOp
+    [OpCode(ILOpCode.Code.Ldlen)]
+    public class Ldlen : ILOp
+    {
+        public Ldlen(Assembler aAsmblr)
+            : base(aAsmblr)
+        {
+        }
+
+        public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
+        {
+            Assemble(Assembler, DebugEnabled);
+        }
+
+        public static void Assemble(Assembler aAssembler, bool debugEnabled, bool doNullReferenceCheck = true)
+        {
+            if (doNullReferenceCheck)
             {
-                public Ldlen(Assembler aAsmblr)
-                    : base(aAsmblr)
-                {
-                }
-
-                public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
-                {
-                    Assemble(Assembler, DebugEnabled);
-                }
-
-                public static void Assemble(Assembler aAssembler, bool debugEnabled, bool doNullReferenceCheck = true)
-                {
-                    if (doNullReferenceCheck)
-                    {
-                        DoNullReferenceCheck(aAssembler, debugEnabled, 4);
-                    }
-
-                    XS.Add(ESP, 4);
-                    XS.Pop(EAX);
-
-                    XS.Push(EAX, displacement: 8);
-                }
+                DoNullReferenceCheck(aAssembler, debugEnabled, 4);
             }
+
+            XS.Add(ESP, 4);
+            XS.Pop(EAX);
+
+            XS.Push(EAX, displacement: 8);
+        }
+    }
 }
