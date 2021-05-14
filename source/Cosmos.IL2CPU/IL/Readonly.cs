afb3985ff482610a340b1d9723ee7c93868ef51d
Quajak <jasper.gerigk@gmx.net>
2021-05-14 09:40:47 +0200

    Implement readonly
    It should be fine for now to do nothing in readonly

--- source/Cosmos.IL2CPU/IL/Readonly.cs
+++ source/Cosmos.IL2CPU/IL/Readonly.cs
@@ -14,7 +14,8 @@ namespace Cosmos.IL2CPU.X86.IL
 
         public override void Execute(_MethodInfo aMethod, ILOpCode aOpCode)
         {
-            throw new NotImplementedException();
+            XS.Comment("Readonly - for now do nothing");
+            XS.Noop();
         }
     }
 }
