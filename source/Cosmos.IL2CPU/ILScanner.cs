0ab39656e152cadc4d75c8896d00394886bbe426
xafero <xafero@outlook.com>
2021-10-01 03:46:22 +0200

    Using the same comparer as the set itself

--- source/Cosmos.IL2CPU/ILScanner.cs
+++ source/Cosmos.IL2CPU/ILScanner.cs
@@ -350,7 +350,7 @@ namespace Cosmos.IL2CPU
             {
                 if (xOpCode is ILOpCodes.OpMethod xOpMethod)
                 {
-                    mItems.TryGetMyValue(xOpMethod.Value, out MemberInfo value);
+                    mItems.TryGetMyValue(xOpMethod.Value, out MemberInfo value, mItems.Comparer);
                     xOpMethod.Value = (MethodBase)(value ?? xOpMethod.Value);
                     xOpMethod.ValueUID = GetMethodUID(xOpMethod.Value);
                 }
