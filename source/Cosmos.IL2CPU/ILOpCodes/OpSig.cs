d2610c88f144b5085f9cf6c3c8c6b47ca5fb6842
Quajak <jasper.gerigk@gmx.net>
2022-02-21 20:27:40 +0100

    Implement calli

--- source/Cosmos.IL2CPU/ILOpCodes/OpSig.cs
+++ source/Cosmos.IL2CPU/ILOpCodes/OpSig.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using System.Reflection;
 
-namespace Cosmos.IL2CPU.ILOpCodes {
-  public class OpSig : ILOpCode {
-    public int Value { get; }
-
-    public OpSig(Code aOpCode, int aPos, int aNextPos, int aValue, _ExceptionRegionInfo aCurrentExceptionRegion)
-      : base(aOpCode, aPos, aNextPos, aCurrentExceptionRegion) {
-      Value = aValue;
-    }
-
-    public override int GetNumberOfStackPops(MethodBase aMethod)
+namespace Cosmos.IL2CPU.ILOpCodes
+{
+    public class OpSig : ILOpCode
     {
-      switch (OpCode)
-      {
-        default:
-          throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
-      }
-    }
+        public int Value { get; }
 
-    public override int GetNumberOfStackPushes(MethodBase aMethod)
-    {
-      switch (OpCode)
-      {
-        default:
-          throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
-      }
-    }
+        public OpSig(Code aOpCode, int aPos, int aNextPos, int aValue, _ExceptionRegionInfo aCurrentExceptionRegion)
+          : base(aOpCode, aPos, aNextPos, aCurrentExceptionRegion)
+        {
+            Value = aValue;
+        }
 
-    public override void DoInterpretStackTypes() { }
-  }
+        public override int GetNumberOfStackPops(MethodBase aMethod)
+        {
+            switch (OpCode)
+            {
+                case Code.Calli:
+                    return 1;
+                default:
+                    throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
+            }
+        }
+
+        public override int GetNumberOfStackPushes(MethodBase aMethod)
+        {
+            switch (OpCode)
+            {
+                case Code.Calli:
+                    return 0;
+                default:
+                    throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
+            }
+        }
+
+        public override void DoInterpretStackTypes() { }
+    }
 }
