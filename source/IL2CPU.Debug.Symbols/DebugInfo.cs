c6d0b64f610e00e37ec9c092c3ae6dd481b87227
TheFocusMan <sdshai2004@gmail.com>
2022-10-23 18:41:41 +0300

    Self upadting because problems with github

--- source/IL2CPU.Debug.Symbols/DebugInfo.cs
+++ source/IL2CPU.Debug.Symbols/DebugInfo.cs
@@ -77,17 +77,18 @@ namespace IL2CPU.Debug.Symbols
             // Use the SQLiteConnectionFactory as the default database connection
             // Do not open mConnection before mEntities.CreateDatabase
             var xDir = IntPtr.Size == 4 ? "x86" : "x64";
-            var os = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" : "linux";
-            char path_separator = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ';' : ':';
+            var os = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)? "win" : "linux";
+            char path_separator = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)? ';' : ':';
 
             Environment.SetEnvironmentVariable("PATH", // add path so that it finds SQLitePCLRaw.nativelibrary
                 String.Join(path_separator.ToString(), Environment.GetEnvironmentVariable("PATH"),
                 Path.Combine(
-                    Path.GetDirectoryName(typeof(DebugInfo).Assembly.Location),
+                    Path.GetDirectoryName(typeof(DebugInfo).Assembly.Location), 
                     $"runtimes/{os}-{xDir}/native"
                     )
                 )
             );
+
             SQLitePCL.Batteries.Init();
             initConnection = new SqliteConnection(mConnStr);
             initConnection.Open();
