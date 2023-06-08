ba49e1668e3ec0d24359f464e895e6351da47ab8
Terminal.cs <evanlawrence@cyberfear.com>
2023-06-07 17:34:44 -0700

    Refactor 2

--- source/IL2CPU.Debug.Symbols/SqliteBulkCopy.cs
+++ source/IL2CPU.Debug.Symbols/SqliteBulkCopy.cs
@@ -1,26 +1,13 @@
 ﻿using Microsoft.Data.Sqlite;
-using System;
 using System.Data;
+using System.Text;
+using System;
 
 namespace IL2CPU.Debug.Symbols
 {
     public class SqliteBulkCopy : IDisposable
     {
-        private bool mDisposed = false;
-
-        public void Dispose()
-        {
-            if (mDisposed)
-            {
-                return;
-            }
-            mDisposed = true;
-            GC.SuppressFinalize(this);
-        }
-
-        private readonly SqliteTransaction mTransaction;
-        private readonly SqliteConnection mConnection;
-        private readonly SqliteCommand mCommand;
+        #region Constructors
 
         public SqliteBulkCopy(SqliteConnection connection)
         {
@@ -28,9 +15,19 @@ namespace IL2CPU.Debug.Symbols
             mTransaction = mConnection.BeginTransaction();
             mCommand = mConnection.CreateCommand();
             mCommand.Transaction = mTransaction;
+            mFieldNames = new StringBuilder();
+            mParamNames = new StringBuilder();
         }
 
+        #endregion
+
+        #region Properties
+
         public string DestinationTableName { get; set; }
+
+        #endregion
+
+        #region Methods
 
         public void WriteToServer(IDataReader reader)
         {
@@ -38,22 +35,22 @@ namespace IL2CPU.Debug.Symbols
             {
                 // initialize bulk copy
 
-                var fieldNames = "";
-                var paramNames = "";
+                mFieldNames.Clear();
+                mParamNames.Clear();
+
                 SqliteParameter[] parms = new SqliteParameter[reader.FieldCount];
                 for (int i = 0; i < reader.FieldCount; i++)
                 {
                     string xFieldName = reader.GetName(i);
-                    fieldNames += $"{xFieldName},";
-                    paramNames += $"@_{xFieldName},";
+                    mFieldNames.Append($"{xFieldName},");
+                    mParamNames.Append($"@_{xFieldName},");
                     parms[i] = new SqliteParameter($"@_{xFieldName}", SqliteType.Text);
                     mCommand.Parameters.Add(parms[i]);
                 }
-                fieldNames = fieldNames.TrimEnd(',');
-                paramNames = paramNames.TrimEnd(',');
 
-                mCommand.CommandText = $"insert into [{DestinationTableName}] ({fieldNames}) values ({paramNames})";
+                mCommand.CommandText = $"insert into [{DestinationTableName}] ({mFieldNames.ToString().TrimEnd(',')}) values ({mParamNames.ToString().TrimEnd(',')})";
                 mCommand.Prepare();
+
                 while (reader.Read())
                 {
                     for (int i = 0; i < reader.FieldCount; i++)
@@ -65,8 +62,32 @@ namespace IL2CPU.Debug.Symbols
                     }
                     mCommand.ExecuteNonQuery();
                 }
+
                 mTransaction.Commit();
             }
         }
+
+        public void Dispose()
+        {
+            if (mDisposed)
+            {
+                return;
+            }
+            mDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly SqliteTransaction mTransaction;
+        private readonly SqliteConnection mConnection;
+        private readonly StringBuilder mFieldNames;
+        private readonly StringBuilder mParamNames;
+        private readonly SqliteCommand mCommand;
+        private bool mDisposed = false;
+
+        #endregion
     }
 }
