caca25b3ca44a788e5c0ab765d1d21bf62f4e6ca
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-06-26 00:30:44 +0200

    Fix tests

--- tests/IL2CPU.Compiler.Tests/ILStackAnalysisTests.cs
+++ tests/IL2CPU.Compiler.Tests/ILStackAnalysisTests.cs
@@ -71,7 +71,7 @@ namespace IL2CPU.Compiler.Tests
             var method = aType.GetMethod(aMethodName, 0, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, aArgs, null);
             var methodBase = new Il2cpuMethodInfo(method, 1, Il2cpuMethodInfo.TypeEnum.Normal, null);
 
-            var appAssembler = new AppAssembler(null, new VoidTextWriter(), "")
+            var appAssembler = new AppAssembler(null, new VoidTextWriter(), "", "")
             {
                 DebugMode = Cosmos.Build.Common.DebugMode.None
             };
@@ -105,7 +105,7 @@ namespace IL2CPU.Compiler.Tests
             var method = aType.GetMethod(aMethodName, 0, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, aArgs, null);
             var methodBase = new Il2cpuMethodInfo(method, 1, Il2cpuMethodInfo.TypeEnum.Normal, null);
 
-            var appAssembler = new AppAssembler(null, new VoidTextWriter(), "")
+            var appAssembler = new AppAssembler(null, new VoidTextWriter(), "", "")
             {
                 DebugMode = Cosmos.Build.Common.DebugMode.None
             };
