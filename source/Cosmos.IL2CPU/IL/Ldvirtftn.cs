f206a2d9f4b2bc4e5eccfeef09c501d90067cd10
Mikhail <mishakeys20@gmail.com>
2024-08-17 11:24:20 -0400

    Try implementing Ldvirtftn

--- source/Cosmos.IL2CPU/IL/Ldvirtftn.cs
+++ source/Cosmos.IL2CPU/IL/Ldvirtftn.cs
@@ -1,5 +1,7 @@
 using System;
-
+using XSharp;
+using Cosmos.IL2CPU.ILOpCodes;
+using IL2CPU.API;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
@@ -10,37 +12,8 @@ namespace Cosmos.IL2CPU.X86.IL
 		{
 		}
 
-    public override void Execute(Il2cpuMethodInfo aMethod, ILOpCode aOpCode) {
-        DoNullReferenceCheck(Assembler, DebugEnabled, 0);
-        throw new NotImplementedException();
-    }
-
-    
-		// using System;
-		// using System.IO;
-		// 
-		// 
-		// using CPU = XSharp.Assembler.x86;
-		// 
-		// namespace Cosmos.IL2CPU.IL.X86 {
-		// 	[XSharp.Assembler.OpCode(OpCodeEnum.Ldvirtftn)]
-		// 	public class Ldvirtftn: Op {
-		//         private string mNextLabel;
-		// 	    private string mCurLabel;
-		// 	    private uint mCurOffset;
-		// 	    private MethodInformation mMethodInformation;
-		// 		public Ldvirtftn(ILReader aReader, MethodInformation aMethodInfo)
-		// 			: base(aReader, aMethodInfo) {
-		//              mMethodInformation = aMethodInfo;
-		// 		    mCurOffset = aReader.Position;
-		// 		    mCurLabel = IL.Op.GetInstructionLabel(aReader);
-		//             mNextLabel = IL.Op.GetInstructionLabel(aReader.NextPosition);
-		// 		}
-		// 		public override void DoAssemble() {
-		//             EmitNotImplementedException(Assembler, GetServiceProvider(), "Ldvirtftn: This has not been implemented at all yet!", mCurLabel, mMethodInformation, mCurOffset, mNextLabel);
-		// 		}
-		// 	}
-		// }
-		
+		public override void Execute(Il2cpuMethodInfo aMethod, ILOpCode aOpCode) {
+        	XS.Push(LabelName.Get(((OpMethod)aOpCode).Value));
+		}
 	}
 }
